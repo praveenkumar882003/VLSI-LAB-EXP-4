767d57c1e499cda124dbea2d885e66c3843d39a7
praveenkumar882003 <164858953+praveenkumar882003@users.noreply.github.com>
2024-05-11 13:34:41 +0530

    Exp4_JKflipflop.v

--- Exp4_JKflipflop.v
+++ Exp4_JKflipflop.v
@@ -1,8 +1,19 @@
-6(ii): JK FLIP FLOP 
-module jk( j,k,clk,q); 
-input j,k,clk; 
-output reg q=0; 
 
-
-
+module jkff(j,k,clk,rst,q);
+input j,k,clk,rst;
+output reg q;
+always@(posedge clk)
+begin
+if(rst==1)
+q=0;
+else
+begin
+case({j,k})
+2'b00:q=q;
+2'b01:q=0;
+2'b10:q=1;
+2'b11:q=~q;
+endcase
+end
+end
 endmodule
