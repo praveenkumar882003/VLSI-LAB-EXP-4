fed14d3e3efb84bae438e60a4e3b886cea5f6e06
praveenkumar882003 <164858953+praveenkumar882003@users.noreply.github.com>
2024-05-11 13:38:38 +0530

    Exp4_Mod10counter.v

--- Exp4_Mod10counter.v
+++ Exp4_Mod10counter.v
@@ -1,12 +1,11 @@
-//MOD -10 COUNTER 
-module mod10(rst,clk,out); 
-input rst,clk; 
-output reg[3:0]out=4'b0000;
- always@ (posedge clk) 
-begin 
-if(rst==1 | out==4'b1001) 
-out=4'b0000; 
-else 
-out=out+4'b0001;
- end 
+module mod10(clk,rst,out);
+input clk,rst;
+output reg [3:0]out;
+always@(posedge clk)
+begin
+if (rst==1 | out==4'b1001)
+out=4'b0000;
+else
+out=out+1;
+end
 endmodule
